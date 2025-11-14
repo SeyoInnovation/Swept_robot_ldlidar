710ac1ecc9a7de07c03eaad6b423c5f7c6f5e185
xiluo <wangzinuo233@gmail.com>
2025-11-14 11:10:29 +0800

    add readme

--- launch/ld14p.launch.py
+++ launch/ld14p.launch.py
@@ -2,28 +2,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
-
 '''
-Parameter Description:
+参数描述：
 ---
-- Set laser scan directon: 
-  1. Set counterclockwise, example: {'laser_scan_dir': True}
-  2. Set clockwise,        example: {'laser_scan_dir': False}
-- Angle crop setting, Mask data within the set angle range:
-  1. Enable angle crop fuction:
-    1.1. enable angle crop,  example: {'enable_angle_crop_func': True}
-    1.2. disable angle crop, example: {'enable_angle_crop_func': False}
-  2. Angle cropping interval setting:
-  - The distance and intensity data within the set angle range will be set to 0.
-  - angle >= 'angle_crop_min' and angle <= 'angle_crop_max' which is [angle_crop_min, angle_crop_max], unit is degress.
-    example:
+- 设置激光扫描方向：
+  1. 设置逆时针，例如：{'laser_scan_dir': True}
+  2. 设置顺时针，例如：{'laser_scan_dir': False}
+- 角度裁剪设置，在设置的角度范围内屏蔽数据：
+  1. 启用角度裁剪功能：
+    1.1. 启用角度裁剪，例如：{'enable_angle_crop_func': True}
+    1.2. 禁用角度裁剪，例如：{'enable_angle_crop_func': False}
+  2. 角度裁剪区间设置：
+  - 在设置的角度范围内的距离和强度数据将被设置为 0。
+  - 角度 >= 'angle_crop_min' 且角度 <= 'angle_crop_max'，即 [angle_crop_min, angle_crop_max]，单位为度。
+    示例：
       {'angle_crop_min': 135.0}
       {'angle_crop_max': 225.0}
-      which is [135.0, 225.0], angle unit is degress.
+      即 [135.0, 225.0]，角度单位为度。
 '''
-
 def generate_launch_description():
-  # LDROBOT LiDAR publisher node
+  # LDROBOT 激光雷达发布节点
   ldlidar_node = Node(
       package='ldlidar',
       executable='ldlidar',
@@ -32,7 +30,7 @@ def generate_launch_description():
       parameters=[
         {'product_name': 'LDLiDAR_LD14P'},
         {'topic_name': 'scan'},
-        {'port_name': '/dev/ttyACM0'},
+        {'port_name': '/dev/ttyACM0'}, # 串口
         {'frame_id': 'base_laser'},
         {'laser_scan_dir': True},
         {'enable_angle_crop_func': False},#单角度裁剪开关：值为False时表示不使用多角度裁剪，默认为False
@@ -41,25 +39,19 @@ def generate_launch_description():
         {'truncated_mode_': 0}#值为1表示使用多角度裁剪，同时enable_angle_crop_func设为False，角度值在/main.cpp中修改
       ]
   )
-
-  # base_link to base_laser tf node
+  # base_link 到 base_laser 的 TF 节点
   base_link_to_laser_tf_node = Node(
     package='tf2_ros',
     executable='static_transform_publisher',
     name='base_link_to_base_laser_ld14p',
     arguments=['0','0','0.18','0','0','0','base_link','base_laser']
   )
-
   scan_fre_node = ExecuteProcess(
     cmd=['ros2','run','ldlidar','LD14P_scan_fre.py']
   )
-
-
-  # Define LaunchDescription variable
+  # 定义 LaunchDescription 变量
   ld = LaunchDescription()
-
-  #ld.add_action(scan_fre_node)  #<!--调节雷达扫描频率，scan_fre扫描频率与雷达串口号请在LD14P_scan_fre.py文件中修改-->
+  #ld.add_action(scan_fre_node) #<!--调节雷达扫描频率，scan_fre扫描频率与雷达串口号请在LD14P_scan_fre.py文件中修改-->
   ld.add_action(ldlidar_node)
   ld.add_action(base_link_to_laser_tf_node)
-
   return ld
